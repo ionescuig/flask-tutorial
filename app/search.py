910522ecfcb40e4154e9a2578df04fd4ed12adc2
Gabriel Ionescu <ionescuig@yahoo.com>
2021-01-07 17:04:22 +0000

    Added print statements to test elasticsearch errors

--- app/search.py
+++ app/search.py
@@ -1,27 +1,36 @@
 from flask import current_app
 
 def add_to_index(index, model):
-    print('>>> [add_to_index] >>>')
+    print('--------------- [add_to_index] ---------------')
     if not current_app.elasticsearch:
+        print('--------------- no current_app.elasticsearch ---------------')
+        print('--------------- [END OF add_to_index] ---------------')
         return
     payload = {}
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
+    print('--------------- [END OF add_to_index] ---------------')
     current_app.elasticsearch.index(index=index, id=model.id, body=payload)
 
 def remove_from_index(index, model):
-    print('>>> [remove_from_index] >>>')
+    print('--------------- [remove_from_index] ---------------')
     if not current_app.elasticsearch:
+        print('--------------- no current_app.elasticsearch ---------------')
+        print('--------------- [END OF remove_from_index] ---------------')
         return
+    print('--------------- [END OF remove_from_index] ---------------')
     current_app.elasticsearch.delete(index=index, id=model.id)
 
 def query_index(index, query, page, per_page):
-    print('>>> [query_index] >>>')
+    print('--------------- [query_index] ---------------')
     if not current_app.elasticsearch:
+        print('--------------- no current_app.elasticsearch ---------------')
+        print('--------------- [END OF query_index] ---------------')
         return [], 0
     search = current_app.elasticsearch.search(
         index=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
               'from': (page - 1) * per_page, 'size': per_page})
     ids = [int(hit['_id']) for hit in search['hits']['hits']]
+    print('--------------- [END OF query_index] ---------------')
     return ids, search['hits']['total']['value']
